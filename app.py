6f66874e75a25afa79b8053718a20a2549d60436
Rich <40706106+Richard-2020@users.noreply.github.com>
2024-11-22 12:16:22 -0500

    commiting, took out if main

--- app.py
+++ app.py
@@ -104,14 +104,14 @@ async def read_root():
     return FileResponse("home.html")
 
 
-if __name__ == "__main__":
-    import uvicorn
-    import os
+# if __name__ == "__main__":
+#     import uvicorn
+#     import os
     
-    # Get port from Heroku environment variable or default to 8000
-    port = int(os.environ.get("PORT", 8000))
+#     # Get port from Heroku environment variable or default to 8000
+#     port = int(os.environ.get("PORT", 8000))
     
-    uvicorn.run(app, host="0.0.0.0", port=port)
+#     uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 
